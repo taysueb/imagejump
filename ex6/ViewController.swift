624a6e80520ea506a23f4b3fb43cf42e72dd50b5
taysueb <tay.chaicharn@gmail.com>
2017-09-21 11:42:58 +0700

    imagejump

--- ex6/ViewController.swift
+++ ex6/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Slider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var Button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var imageList:[UIImage] = []
+        for i in 1...20{
+            let fn = "frame-"+String(format: "%d",i)+".png"
+            let vImage = UIImage(named: fn)
+            imageList.append(vImage!) // load pic to animation image
+            
+        }
+        imageView.animationImages = imageList
+        imageView.animationDuration = 1
     }
 
+    @IBAction func StartStopMethod(_ sender: Any) {
+        if imageView.isAnimating{
+            imageView.stopAnimating()
+            Button.setTitle("sit!",for: UIControlState.normal)
+        }else{
+            imageView.startAnimating()
+            Button.setTitle("jump!",for: UIControlState.normal)
+        }
+    }
+   
+    @IBAction func speedMethod(_ sender: Any) {
+        imageView.animationDuration = TimeInterval(2-Slider.value)
+        imageView.startAnimating()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
